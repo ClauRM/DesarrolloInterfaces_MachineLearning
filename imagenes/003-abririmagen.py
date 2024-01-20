ee3bbbdfd114a4ea2795ec12994da01e92a7d7d3
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-01-20 14:07:14 +0100

    Acceder a una propiedad de la imagen

---
+++ imagenes/003-abririmagen.py
@@ -0,0 +1,15 @@
+from PIL import Image #cargar la libreria de imagenes
+import os #leer dentro de una carpeta
+
+carpeta = "001-crudo"
+
+archivos = os.listdir(carpeta) #listar ficheros dentro de la carpeta
+
+for archivo in archivos:
+    imagen = os.path.join(carpeta,archivo)#unir la carpeta con el archivo para calcular la ruta
+    print(imagen)
+    miimagen = Image.open(imagen)#abro la imagen
+    print(miimagen.width)#acceso a una de sus propiedades (anchura)
+    
+
+
