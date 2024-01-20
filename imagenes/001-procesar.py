b36e39050d2dfc36a588995a923152a73ce9abb6
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-01-20 14:00:18 +0100

    Leer imágenes desde una carpeta

---
+++ imagenes/001-procesar.py
@@ -0,0 +1,11 @@
+from PIL import Image #cargar la libreria de imagenes
+import os #leer dentro de una carpeta
+
+carpeta = "001-crudo"
+
+archivos = os.listdir(carpeta) #listar ficheros dentro de la carpeta
+
+print(archivos)
+    
+
+
