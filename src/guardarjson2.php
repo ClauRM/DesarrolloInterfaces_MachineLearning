a2dd09a28c3c18cd0d911ac073918d235410c24e
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-01-20 12:25:44 +0100

    Añade información estadística al fichero json

---
+++ src/guardarjson2.php
@@ -0,0 +1,19 @@
+<?php
+
+$archivo = "../datos/prueba.json";
+
+$datos = [ //lo recibe del fichero javascript por el metodo get
+    'elemento' => $_GET ['patron'],
+    'datos'=> $_GET['datos']
+];
+
+$datosexistentes =file_get_contents($archivo);
+$datosexistentesjson = json_decode($datosexistentes);
+
+array_push($datosexistentesjson,$datos);
+
+$json = json_encode($datosexistentesjson, JSON_PRETTY_PRINT);
+
+file_put_contents($archivo,$json);
+
+?>
