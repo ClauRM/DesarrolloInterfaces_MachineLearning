57da299200252a79a474516b072f545fa80922ff
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2024-01-21 19:52:15 +0100

    Método calcularPatrones

--- code/022-codigoescalable.js
+++ code/022-codigoescalable.js
@@ -13,6 +13,7 @@ var contextovertical;
 var contextohorizontal;
 var contextodiagonal1;
 var contextodiagonal2;
+var arrayContextos = []; //array de contextos
 //crear objeto imagen
 var imagen = new Image();
 
@@ -39,7 +40,7 @@ window.onload = function () {
     cuentapatrones[1] = 0;
     cuentapatrones[2] = 0;
     cuentapatrones[3] = 0;
-    
+
     //bucle asincronico para procesar las imagenes
     fetch("../images/json/imagenes.json")
         .then(function (response) {
@@ -51,7 +52,7 @@ window.onload = function () {
         });
 }
 
-//bucle temporizador
+//bucle temporizador que se ejecuta cada 5s
 function bucle() {
     procesaImagen("../images/002-procesadas/" + datos[supercontador]);
     clearTimeout(temporizador);
@@ -64,11 +65,11 @@ function procesaImagen(miimagen) {
     imagen.src = miimagen; //carga una imagen
     //cuando la imagen cargue ejecutar funcion
     imagen.onload = function () {
-        //dibujar los patrones de linea
-        contextovertical.drawImage(patrones[0],0,0)
-        contextohorizontal.drawImage(patrones[1],0,0)
-        contextodiagonal1.drawImage(patrones[2],0,0)
-        contextodiagonal2.drawImage(patrones[3],0,0)
+        //dibujar los contextos de lineas patron
+        contextovertical.drawImage(patrones[0], 0, 0)
+        contextohorizontal.drawImage(patrones[1], 0, 0)
+        contextodiagonal1.drawImage(patrones[2], 0, 0)
+        contextodiagonal2.drawImage(patrones[3], 0, 0)
         //dibujar la imagen original en coordenadas 0,0 sobre el lienzo1
         contexto1.drawImage(imagen, 0, 0);
         //cargar las imagenes para detectar sus bordes
@@ -94,81 +95,13 @@ function procesaImagen(miimagen) {
         }
         //pintar los contornos en el lienzo2
         contexto2.putImageData(imagenlienzo2, 0, 0);
-        //recorrer la ultima imagen con contornos en vertical /////////
-        let muestravertical = contextovertical.getImageData(0, 0, 8, 8); //tomar los datos desde 0,0 hasta 8,8
-        for (let x = 0; x < 512; x++) {
-            for (let y = 0; y < 512; y++) {
-                //por cada pixel comprobar si tiene el patron
-                let trozo = contexto2.getImageData(x, y, 8, 8); //8x8 tamanio del patron
-                let suma = 0;
-                for (let i = 0; i < trozo.data.length; i += 4) { //4 informacion del trozo
-                    suma += Math.abs(trozo.data[i] - muestravertical.data[i]);
-                }
-                //detectar los pixeles verticales
-                if (suma < 4000) {
-                    cuentapatrones[0]++; //lineas verticales
-                    contexto3.fillStyle = "red";
-                    contexto3.fillRect(x, y, 2, 2);
-                }
-            }
-        }
-        //recorrer la ultima imagen con contornos en horizontal /////////
-        let muestrahorizontal = contextohorizontal.getImageData(0, 0, 8, 8); //tomar los datos desde 0,0 hasta 8,8
-        for (let x = 0; x < 512; x++) {
-            for (let y = 0; y < 512; y++) {
-                //por cada pixel comprobar si tiene el patron
-                let trozo = contexto2.getImageData(x, y, 8, 8); //8x8 tamanio del patron
-                let suma = 0;
-                for (let i = 0; i < trozo.data.length; i += 4) { //4 informacion del trozo
-                    suma += Math.abs(trozo.data[i] - muestrahorizontal.data[i]);
-                }
-                //detectar los pixeles verticales
-                if (suma < 4000) {
-                    cuentapatrones[1]++;//lineas horizontales
-                    contexto3.fillStyle = "blue";
-                    contexto3.fillRect(x, y, 2, 2);
-                }
-            }
-        }
-        //recorrer la ultima imagen con contornos en diagonal1 /////////
-        let muestradiagonal1 = contextodiagonal1.getImageData(0, 0, 8, 8); //tomar los datos desde 0,0 hasta 8,8
-        for (let x = 0; x < 512; x++) {
-            for (let y = 0; y < 512; y++) {
-                //por cada pixel comprobar si tiene el patron
-                let trozo = contexto2.getImageData(x, y, 8, 8); //8x8 tamanio del patron
-                let suma = 0;
-                for (let i = 0; i < trozo.data.length; i += 4) { //4 informacion del trozo
-                    suma += Math.abs(trozo.data[i] - muestradiagonal1.data[i]);
-                }
-                //detectar los pixeles verticales
-                if (suma < 4000) {
-                    cuentapatrones[2]++; //lineas diagonales1
-                    contexto3.fillStyle = "green";
-                    contexto3.fillRect(x, y, 2, 2);
-                }
-            }
-        }
-        //recorrer la ultima imagen con contornos en vertical /////////
-        let muestradiagonal2 = contextodiagonal2.getImageData(0, 0, 8, 8); //tomar los datos desde 0,0 hasta 8,8
-        for (let x = 0; x < 512; x++) {
-            for (let y = 0; y < 512; y++) {
-                //por cada pixel comprobar si tiene el patron
-                let trozo = contexto2.getImageData(x, y, 8, 8); //8x8 tamanio del patron
-                let suma = 0;
-                for (let i = 0; i < trozo.data.length; i += 4) { //4 informacion del trozo
-                    suma += Math.abs(trozo.data[i] - muestradiagonal2.data[i]);
-                }
-                //detectar los pixeles verticales
-                if (suma < 4000) {
-                    cuentapatrones[3]++; //lineas diagonales2
-                    contexto3.fillStyle = "orange";
-                    contexto3.fillRect(x, y, 2, 2);
-                }
-            }
-        }
+
+        //calcular numero de lineas por cada patron y almacenar en array cuentapatrones
+        arrayContextos = [contextovertical, contextohorizontal, contextodiagonal1, contextodiagonal2];
+        cuentapatrones = calcularPatrones(arrayContextos, contexto2);
+        
         //estadisticas del numero de lineas horizontales y verticales
         //En el array, posicion 0 lineas verticales, posicion 1 lineas horizontales
-        console.log(cuentapatrones);
         //convertir totales a porcentajes
         let total = 0;
         for (let i = 0; i < cuentapatrones.length; i++) {
@@ -190,4 +123,33 @@ function procesaImagen(miimagen) {
         let nombrepatron = arraypatron[arraypatron.length - 1];
         fetch("guardarjson2.php?archivo=" + nombrefichero + "&patron=" + nombrepatron + "&datos=" + datos);
     }
+}
+
+//metodo para calcular el numero de lineas por cada patron
+function calcularPatrones(arrayContextos, contexto2) {
+    //para cada linea patron contenida en el array de contextos, ejecutar
+    for (let i = 0; i < arrayContextos.length; i++) {
+        //calcular color aleatorio para identificar un tipo de patron
+        let color = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+        //tomar una muestra de datos desde 0,0 hasta 8,8 (tamanio en pixeles del patron)
+        let muestra = arrayContextos[i].getImageData(0, 0, 8, 8);
+        for (let x = 0; x < 512; x++) {
+            for (let y = 0; y < 512; y++) {
+                //por cada pixel comprobar si tiene el patron
+                let trozoImagen2 = contexto2.getImageData(x, y, 8, 8); //8x8 tamanio del patron
+                let suma = 0;
+                for (let j = 0; j < trozoImagen2.data.length; j += 4) { //4 informacion del trozo
+                    suma += Math.abs(trozoImagen2.data[j] - muestra.data[j]);
+                }
+                //si la diferencia de comparacion es pequenia es porque son similares 
+                if (suma < 4000) {
+                    cuentapatrones[i]++; // aumenta la cuenta numero de lineas 
+                    contexto3.fillStyle = color;
+                    contexto3.fillRect(x, y, 2, 2);
+                }
+            }
+        }
+    }
+
+    return cuentapatrones;
 }
